68cfa694c275f90db3632fd21f326b895bffe32e
watermoe <87688603@qq.com>
2024-10-15 03:17:53 +0800

    add layout

---
+++ src/layouts/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">
+    <RouterView v-slot="{ Component }" v-if="$route.meta.keepAlive">
+      <keep-alive>
+        <component :is="Component" :key="$route.path" />
+      </keep-alive>
+    </RouterView>
+    <RouterView v-if="!$route.meta.keepAlive" :key="$route.path" />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>

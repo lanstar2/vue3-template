666072e19a9ab624a376b5c77dd074109d06a0f0
watermoe <87688603@qq.com>
2024-10-15 03:23:39 +0800

    remove el css

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,6 @@ import { useUserStore } from './store'
 import store from './store'
 import './assets/css/index.css'
 import 'animate.css'
-// 单独引入 ElMessage 和 ElMessageBox 的样式
-import 'element-plus/theme-chalk/el-message.css'
-import 'element-plus/theme-chalk/el-message-box.css'
 const app = createApp(App).use(store)
 
 // 获取基础数据
